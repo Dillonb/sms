5d469bedf177f6fecfcc004a100669823b0ae97a
Dillon Beliveau <dillonbeliveau@gmail.com>
2021-11-20 15:35:52 -0800

    allow running main emulator with both CPU implementations

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,53 @@
 #include "z80/z80.h"
 #include "util/log.h"
 #include "vdp/vdp.h"
+#include "../test/superzazu_z80/z80.h"
 
-int main(int argc, char** argv) {
-    Rom::load(argv[1]);
 
+static z80 z;
+
+u8 sz_read_byte(void*, u16 address) {
+    return Bus::read_byte(address);
+}
+
+void sz_write_byte(void*, u16 address, u8 value) {
+    Bus::write_byte(address, value);
+}
+
+u8 sz_port_in(z80*, u8 port) {
+    return Bus::port_in(port);
+}
+
+void sz_port_out(z80*, u8 port, u8 value) {
+    Bus::port_out(port, value);
+}
+
+void run_sz() {
+    z80_init(&z);
+    z.write_byte = sz_write_byte;
+    z.read_byte = sz_read_byte;
+    z.port_in = sz_port_in;
+    z.port_out = sz_port_out;
+    Vdp::reset();
+    if (Bios::try_load()) {
+        logalways("Found a bios!");
+    } else {
+        logalways("No bios found.");
+    }
+
+    Vdp::render_init();
+
+    while (1) {
+        if (Vdp::interrupt_pending()) {
+            z.int_pending = true;
+        }
+        z80_step(&z);
+        Vdp::step(z.cyc);
+        z.cyc = 0;
+    }
+}
+
+void run_mine() {
     Z80::reset();
     Vdp::reset();
     Z80::set_bus_handlers(Bus::read_byte, Bus::write_byte);
@@ -29,6 +72,12 @@ int main(int argc, char** argv) {
         int cycles = Z80::step();
         Vdp::step(cycles);
     }
+}
+
+int main(int argc, char** argv) {
+    Rom::load(argv[1]);
+
+    run_mine();
 
     return 0;
 }
